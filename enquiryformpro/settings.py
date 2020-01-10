041200a8b0415db43b55f0be53111b1196a6bfde
kk659419 <59555646+kk659419@users.noreply.github.com>
2020-01-10 08:46:14 +0530

    Update settings.py

--- enquiryformpro/settings.py
+++ enquiryformpro/settings.py
@@ -75,13 +75,10 @@ WSGI_APPLICATION = 'enquiryformpro.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'enquirydb',
-        'USER': 'root',
-        'PASSWORD':'root'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
